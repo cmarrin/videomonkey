886c22501cdb87eba71155b9b8034d3fd3e4bd16
cmarrin <cmarrin@ae59dd93-ecb8-44ce-936a-9b63203e4a5a>
2008-11-11 15:56:55 +0000

    hooking up first conversion params
    git-svn-id: https://videomonkey.svn.sourceforge.net/svnroot/videomonkey/trunk@18 ae59dd93-ecb8-44ce-936a-9b63203e4a5a

--- classes/ConversionParams.h
+++ classes/ConversionParams.h
@@ -1,8 +1,17 @@
 #import <Cocoa/Cocoa.h>
+//#import <Cocoa/NSTabViewItem.h>
+
+@interface ConversionTab : NSTabViewItem {
+    IBOutlet id m_h264Button;
+}
+@end
 
 @interface ConversionParams : NSObject {
-    IBOutlet float *quality;
-    IBOutlet id conversionParamsTabView;
+    IBOutlet float *m_quality;
+    IBOutlet id m_conversionParamsTabView;
+    
+    ConversionTab* m_currentTabViewItem;
 }
 - (IBAction)selectTab:(id)sender;
+- (IBAction)paramChanged:(id)sender;
 @end
--- classes/ConversionParams.m
+++ classes/ConversionParams.m
@@ -1,7 +1,18 @@
 #import "ConversionParams.h"
 
+@implementation ConversionTab
+- (bool) h264 {
+    return [(NSButton*) m_h264Button state] != 0;
+}
+@end
+
 @implementation ConversionParams
 - (IBAction)selectTab:(id)sender {
-    [conversionParamsTabView selectTabViewItem: [sender representedObject]];
+    m_currentTabViewItem = [sender representedObject];
+    [m_conversionParamsTabView selectTabViewItem: m_currentTabViewItem];
+}
+
+- (IBAction)paramChanged:(id)sender {
+    printf("paramChanged: quality=%d\n", (int) [m_currentTabViewItem h264]);
 }
 @end
