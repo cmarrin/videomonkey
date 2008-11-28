928dfc00de33f8e0cc34dcf7c1965a16b0afab76
cmarrin <cmarrin@ae59dd93-ecb8-44ce-936a-9b63203e4a5a>
2008-11-28 16:02:58 +0000

    add instructions on building ffmpeg locally
    git-svn-id: https://videomonkey.svn.sourceforge.net/svnroot/videomonkey/trunk@46 ae59dd93-ecb8-44ce-936a-9b63203e4a5a

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -3,5 +3,5 @@ bunzip2 -kv ffmpeg.tar.bz2
 tar xvf ffmpeg.tar
 rm ffmpeg.tar
 cd ffmpeg
-./configure --disable-ffmpeg --disable-ffplay --disable-ffserver --enable-gpl --enable-postproc --enable-swscale --enable-avfilter --enable-avfilter-lavf --disable-vhook
+./configure --disable-ffmpeg --disable-ffplay --disable-ffserver --enable-gpl --enable-postproc --enable-swscale --enable-avfilter --enable-avfilter-lavf --disable-vhook --enable-libfaac --enable-libfaad --enable-libmp3lame --enable-libvorbis --enable-libtheora --enable-libx264 --enable-libxvid
 make
