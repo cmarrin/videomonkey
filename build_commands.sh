b049a69a8f783ef6097a4e84c28a15f9d8f776c8
cmarrin <cmarrin@ae59dd93-ecb8-44ce-936a-9b63203e4a5a>
2008-12-15 14:24:58 +0000

    added local directory search to opt_presets, so presets can be in bundle
    git-svn-id: https://videomonkey.svn.sourceforge.net/svnroot/videomonkey/trunk@80 ae59dd93-ecb8-44ce-936a-9b63203e4a5a

--- build_commands.sh
+++ build_commands.sh
@@ -3,6 +3,7 @@
 bunzip2 -kv ffmpeg.tar.bz2
 tar xvf ffmpeg.tar
 rm ffmpeg.tar
+cp ffmpeg.c ffmpeg
 cd ffmpeg
 ./configure --disable-ffplay --disable-ffserver --enable-gpl --enable-postproc --enable-swscale --enable-avfilter --enable-avfilter-lavf --disable-vhook --enable-libfaac --enable-libfaad --enable-libmp3lame --enable-libvorbis --enable-libtheora --enable-libx264 --enable-libxvid
 make
