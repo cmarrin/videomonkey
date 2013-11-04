3f036b22620b99c6dab5837c4258572f5de70cd1
Philippe Casgrain <github@casgrain.com>
2013-11-03 23:16:54 -0500

    Added Unit Test bundle

---
+++ VideoMonkey Tests/VideoMonkey_Tests.m
@@ -0,0 +1,34 @@
+//
+//  VideoMonkey_Tests.m
+//  VideoMonkey Tests
+//
+//  Created by Philippe on 2013-11-03.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VideoMonkey_Tests : SenTestCase
+
+@end
+
+@implementation VideoMonkey_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
